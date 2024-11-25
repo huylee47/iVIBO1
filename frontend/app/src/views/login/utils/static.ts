3f9a7efa53c2b20f58a69f287ad5fb232b584b7c
huylee47 <huyw300@gmail.com>
2024-11-25 10:17:59 +0700

    Add light logo

--- frontend/app/src/views/login/utils/static.ts
+++ frontend/app/src/views/login/utils/static.ts
@@ -1,5 +1,7 @@
 import bg from "@/assets/login/bg.png";
 import avatar from "@/assets/login/avatar.svg?component";
+import avatarW from "@/assets/login/avatarW.svg?component";
+
 import illustration from "@/assets/login/illustration.svg?component";
 
-export { bg, avatar, illustration };
+export { bg, avatar, avatarW, illustration };
