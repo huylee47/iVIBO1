24b1ab015275914808284b7f9b950a6278d52ac4
huylee47 <huyw300@gmail.com>
2024-12-20 07:49:28 +0700

    import vi lang

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -16,6 +16,8 @@ import en from "element-plus/es/locale/lang/en";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import plusEn from "plus-pro-components/es/locale/lang/en";
 import plusZhCn from "plus-pro-components/es/locale/lang/zh-cn";
+import vi from "element-plus/es/locale/lang/vi";
+// import plusVi from "plus-pro-components/es/locale/lang/vi";
 
 export default defineComponent({
   name: "app",
@@ -26,8 +28,8 @@ export default defineComponent({
   },
   computed: {
     currentLocale() {
-      return this.$storage.locale?.locale === "zh"
-        ? { ...zhCn, ...plusZhCn }
+      return this.$storage.locale?.locale === "vi"
+        ? { ...vi, ...plusZhCn }
         : { ...en, ...plusEn };
     }
   },
