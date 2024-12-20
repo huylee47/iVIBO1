1c27a52a9e8caf694665ea45052075e33c4ffdf1
huylee47 <huyw300@gmail.com>
2024-12-20 09:27:32 +0700

    create childen route task

--- frontend/app/src/router/modules/task.ts
+++ frontend/app/src/router/modules/task.ts
@@ -7,5 +7,23 @@ export default {
     icon: "ri:task-line",
     title: "Công việc",
     rank: task
-  }
+  },
+  children: [
+    {
+      path: "task/add",
+      name: "taskAdd",
+      component: () => import("@/views/task/add.vue"),
+      meta: {
+        title: "Thêm công việc"
+      }
+    },
+    {
+      path: "/task/index",
+      name: "task",
+      component: () => import("@/views/task/index.vue"),
+      meta: {
+        title: "Công việc"
+      }
+    }
+  ]
 };
